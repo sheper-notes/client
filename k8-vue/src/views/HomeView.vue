95f822e9a80703a42255d2b9b4ff0ff24c64128a
flexiconn <simoncox2003@gmail.com>
2023-09-12 14:30:51 +0200

    tailwind setup

--- k8-vue/src/views/HomeView.vue
+++ k8-vue/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </main>
 </template>
