429d1756e534ea3ff65502bc38f519343307e595
flexiconn <simoncox2003@gmail.com>
2023-12-29 14:01:27 +0100

    added a test for multiple components

--- app/src/views/BoardsView.cy.js
+++ app/src/views/BoardsView.cy.js
@@ -1,6 +1,6 @@
 import BoardsView from './BoardsView.vue'
 
-describe('<BoardsView /> succesful', () => {
+describe('<BoardsView /> renders 1 item', () => {
   it('renders', () => {
     
     // see: https://on.cypress.io/mounting-vue
@@ -13,6 +13,37 @@ describe('<BoardsView /> succesful', () => {
     }).then(() => {
         // Check if the new board item is rendered in the component
         cy.get('.BoardItem').should('have.lengthOf', 1);
+        cy.contains('.BoardItem', 'New Board');
+        
+        
+        cy.get('.Open')
+        .should('have.attr', 'href')
+        .should('not.be.empty')                  // now test the href
+        .and('contain', '/editor/1');
+
+        cy.get('.Settings')
+        .should('have.attr', 'href')
+        .should('not.be.empty')                  // now test the href
+        .and('contain', '/settings/1');
+
+    });
+  })
+})
+
+describe('<BoardsView /> renders multiple item', () => {
+  it('renders', () => {
+    
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(BoardsView, {
+      data() {
+        return {
+          boards: [{ id: 1, name: 'New Board', role: 'Admin' },
+                   { id: 2, name: 'New Board 2', role: 'User' }]
+        };
+      }
+    }).then(() => {
+        // Check if the new board item is rendered in the component
+        cy.get('.BoardItem').should('have.lengthOf', 2);
         cy.contains('.BoardItem', 'New Board');
     });
   })
