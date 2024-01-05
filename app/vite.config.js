03fba6c1fc72bcfd961365760515e6c35ff266ea
Simon Cox <simoncox2003@gmail.com>
2024-01-05 12:07:19 +0100

    Dev (#9)
    * ci/cd changes
    
    * working-directory change
    
    * e2e fix
    
    * ci/cd test fix
    
    * cicd test
    
    * test
    
    * test
    
    * Ci/cd changes (#4)
    
    * ci/cd changes
    
    * working-directory change
    
    * e2e fix
    
    * ci/cd test fix
    
    * cicd test
    
    * test
    
    * test
    
    * changed docker ignore
    
    * Ci/cd changes (#6)
    
    * ci/cd changes
    
    * working-directory change
    
    * e2e fix
    
    * ci/cd test fix
    
    * cicd test
    
    * test
    
    * test
    
    * changed docker ignore
    
    * cypress config ignore fix
    
    * added gdpr info page

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    fs:{
+      strict: true,
+    }
+  },
   plugins: [
     vue(),
   ],
